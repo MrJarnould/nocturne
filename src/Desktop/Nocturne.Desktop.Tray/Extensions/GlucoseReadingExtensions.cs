448a2321e39922fbdcbce6dbad58c2fd386fe728
Rhys <36913739+ryceg@users.noreply.github.com>
2026-04-22 09:33:58 +1000

    refactor: replace raw HTTP and v1 calls with SDK in NocturneClient

---
+++ src/Desktop/Nocturne.Desktop.Tray/Extensions/GlucoseReadingExtensions.cs
@@ -0,0 +1,12 @@
+using NightscoutFoundation.Nocturne.Model;
+
+namespace Nocturne.Desktop.Tray.Extensions;
+
+public static class GlucoseReadingExtensions
+{
+    public static DateTimeOffset GetTimestamp(this V4GlucoseReading reading)
+        => DateTimeOffset.FromUnixTimeMilliseconds(reading.Mills);
+
+    public static TimeSpan GetAge(this V4GlucoseReading reading)
+        => DateTimeOffset.UtcNow - reading.GetTimestamp();
+}
